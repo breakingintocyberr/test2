76b8891bf7c606e031628e1b948bb97d799b6f9b
unknown <jgosnay2201@gmail.com>
2026-01-06 17:58:38 +0100

    main

--- test2.js
+++ test2.js
@@ -1,7 +1,7 @@
 //import http
 const express = require('express')
 const http = require('http')
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 //create server object
 const server = http.createServer((req, res) => {
