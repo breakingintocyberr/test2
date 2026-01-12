522c76ebcf4cf1e9ca1eacd91715fcdcce5a8d5f
unknown <jgosnay2201@gmail.com>
2026-01-12 18:41:53 +0100

    main

--- test2.js
+++ test2.js
@@ -1,3 +1,18 @@
-console.log("Starting test app...");
+//import dependencies
+const http = require('http')
+const port = process.env.PORT || 3000;   
 
-setInterval(() => {}, 1000); // keep process alive
+//create server object
+const server = http.createServer((req, res) => {
+    res.write("Hello from my server!");
+    res.end()
+});
+
+//server listens to port
+server.listen(port, (err) => {
+    if(err) {
+        console.log("Error occured : ", err);
+    } else {
+        console.log("Server started running at : http://localhost:" + port);
+    }
+})
