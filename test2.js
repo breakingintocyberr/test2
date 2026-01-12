daca3a6d1302cd6e325f17cd0bd26050abeb4c40
unknown <jgosnay2201@gmail.com>
2026-01-12 19:23:58 +0100

    main

--- test2.js
+++ test2.js
@@ -1,24 +1,21 @@
 //import dependencies
 const express = require('express')
-const http = require('http')
 const app = express();  
 const port = process.env.PORT || 3000; 
-
-//server listens to port
-server.listen(port, (err) => {
-    if(err) {
-        console.log("Error occured : ", err);
-    } else {
-        console.log("Server started running at : http://localhost:" + port);
-    }
-})
 
 //Serving static files from '/public'
 app.use(express.static('public'));
 
 //Optional route for home page
 app.get('/', (req, res) => {
-    res.sendFile(_dirname + '/public/index.html');
+    res.sendFile(__dirname + '/public/index.html');
 });
 
-
+//server listens to port (starting server)
+app.listen(port, (err) => {
+    if(err) {
+        console.log("Error occured : ", err);
+    } else {
+        console.log("Server started running at : http://localhost:" + port);
+    }
+})
