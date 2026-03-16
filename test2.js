1daca1a9b53480f4021d1225d0870b1afa461216
unknown <jgosnay2201@gmail.com>
2026-03-16 17:46:43 +0100

    main

--- test2.js
+++ test2.js
@@ -1,7 +1,7 @@
 //import dependencies
 const express = require('express')
 const app = express();  
-const port = "localhost:3000"; 
+const port = process.env.PORT || 3000; 
 
 //Serving static files from '/public'
 app.use(express.static('public'));
